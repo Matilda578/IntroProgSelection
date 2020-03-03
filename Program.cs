e30ed395e5d805dc7bf179d58ca2e4f64ba00a87
Matilda578 <101889190@student.swin.edu.au>
2020-03-03 14:16:30 +1000

    pleas work

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IntroProgSelection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 18;
+            int lage= 18;
+
+            if(age > lage) 
+            {
+                Console.WriteLine("drink");
+            } 
+            else
+            {
+                Console.WriteLine("don't dtink");
+            }
+            Console.WriteLine("program finished!");
+        }
+    }
+}
